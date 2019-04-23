bb7e32634f2718f6cfe54934a1fddf843ece49ca
Igor Guastalla de Lima <igor.lima@sabium.com.br>
2019-04-23 10:49:13 -0300

    add component updated

--- src/directive.js
+++ src/directive.js
@@ -1,25 +1,32 @@
+function getValue(el, binding, vnode) {
+  var expression = binding.expression.replace(/['"]+/g, '')
+
+  if (expression && (expression.indexOf('.') > -1)) {
+    var splitted = expression.split('.')
+    var returned = null
+
+    for (var index = 0; index < splitted.length; index++) {
+      var element = splitted[index]
+
+      returned = !returned ? vnode.context[element] : returned[element]
+
+      if (typeof returned !== 'object') {
+        break
+      }
+    }
+
+    el.innerHTML = ((returned !== undefined) && (returned !== null))
+      ? String(returned)
+      : ''
+  }
+}
+
 export default {
   bind(el, binding, vnode) {
-    var expression = binding.expression.replace(/['"]+/g, '')
-
-    if (expression && (expression.indexOf('.') > -1)) {
-      var splitted = expression.split('.')
-      var returned = null
-
-      for (var index = 0; index < splitted.length; index++) {
-        var element = splitted[index]
-
-        returned = !returned ? vnode.context[element] : returned[element]
-
-        if (typeof returned !== 'object') {
-          break
-        }
-      }
-
-      el.innerHTML = ((returned !== undefined) && (returned !== null))
-        ? String(returned)
-        : ''
-    }
+    getValue(el, binding, vnode)
+  },
+  componentUpdated (el, binding, vnode, oldVnode) {
+    getValue(el, binding, vnode)
   },
   unbind (el, binding, vnode, oldVnode) {
     if (el && el.safe) {
