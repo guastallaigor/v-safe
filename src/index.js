0a61de90d38a44a26267d2d03df9f04dec288412
Igor Guastalla de Lima <igor.lima@sabium.com.br>
2019-04-23 10:51:08 -0300

    changed name

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import plugin from './plugin'
 
 export {
   plugin as default,
-  plugin as VueSafePlugin,
-  directive as VueSafeDirective
+  plugin as VSafePlugin,
+  directive as VSafeDirective
 }
